0c52a87a2689a9a7fc7af5a4787defdff985de9a
Angus061855 <165797999+Angus061855@users.noreply.github.com>
2026-08-04 14:39:26 +0800

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -1,5 +1,5 @@
 window.AS_SITE_CONFIG = Object.freeze({
-  officialInstagram: 'https://www.instagram.com/as.mask_/'
+  officialInstagram: 'https://www.instagram.com/no8.post.as/'
 });
 
 document.addEventListener('DOMContentLoaded', function () {
